e5d0d8dd573c3947982c96a0917b74e0124b352a
Stephen Whittier <swhittier@bluetubeinc.com>
2016-07-20 14:31:45 -0400

    Added cmdlets for database backup and restore.

---
+++ Local/PowerShell/modules/BlueTube/Invoke-DBBackup.ps1
@@ -0,0 +1,81 @@
+﻿Function Invoke-DBBackup {
+    
+	[cmdletbinding()]
+		Param(
+			[parameter(Mandatory=$true, ValueFromPipeline)]
+			[ValidateNotNullOrEmpty()] #No value
+			[string]$database
+			)
+            
+	Begin {
+        
+		<#
+			Helper function to show usage of cmdlet.
+		#>
+		Function Show-Usage {
+        
+			Show-InfoMessage "Usage: Invoke-DBBackup -database [db]"        
+			Show-InfoMessage "db: services for Mohawk_Services_Dev Database"
+            Show-InfoMessage "db: commercial for Mohawk_TMGCommercial Database"
+			Show-InfoMessage "db: mongo for Mohawk_Mongo_Data Database"
+		}
+	}
+	Process {
+        
+		$workingDir = (Get-Item -Path ".\" -Verbose).FullName
+		$databaseToBackup = "";
+        $dbBackupStorageRoot = if(![string]::IsNullOrEmpty($env:DBBACKUPSTORAGEROOT)) { $env:DBBACKUPSTORAGEROOT } else { "C:\stuff\DBBackups\" }
+        $dbBackupStorageDir = "";
+
+		switch($database)
+		{
+			"services"
+				{                     
+					$databaseToBackup = "Mohawk_Services_Dev"
+                    $dbBackupStorageDir = "$($dbBackupStorageRoot)Mohawk_Services"
+					break                    
+				}
+            "commercial"
+				{                     
+					$databaseToBackup = "Mohawk_TMGCommercial"
+                    $dbBackupStorageDir = "$($dbBackupStorageRoot)TMGCommercial"
+					break                    
+				}
+            "mongo"
+				{                     
+					$databaseToBackup = "Mohawk_Mongo_Data"
+                    $dbBackupStorageDir = "$($dbBackupStorageRoot)Mohawk_Mongo_Data"
+					break                    
+				}
+            
+
+			default {
+				Show-InfoMessage "Invalid Database"
+				Show-Usage
+				return
+			}
+		}
+
+		Show-InfoMessage ("Backing Up " + $databaseToBackup + " Database...")
+		
+        Add-Type -path "C:\Windows\assembly\GAC_MSIL\Microsoft.SqlServer.Smo\10.0.0.0__89845dcd8080cc91\Microsoft.SqlServer.Smo.dll"
+
+		$sqlServer = New-Object 'Microsoft.SqlServer.Management.SMO.Server' <#$inst#>
+        
+        $sqlServerName = $sqlServer.Name
+		$backupdir = $sqlServer.Settings.BackupDirectory
+		$currentDateTime = get-date -format yyyyMMddHHmmss
+		$backupFileName = "$($databaseToBackup)_$($currentDateTime).bak"
+        $backupFilePath = "$($backupdir)\$($backupFileName)"
+
+		Backup-SqlDatabase -ServerInstance $sqlServerName -Database $databaseToBackup -BackupFile $backupFilePath
+
+		Show-InfoMessage ($databaseToBackup + " Database Back Up Complete.")
+        
+        Show-InfoMessage ("Copying $($backupFilePath) to local storage directory: $($dbBackupStorageDir)")
+
+        Invoke-Expression ("copy $('$backupFilePath') $($dbBackupStorageDir)\$($backupFileName)")
+
+		cd $workingDir        
+	}
+}
---
+++ Local/PowerShell/modules/BlueTube/Invoke-DBRestore.ps1
@@ -0,0 +1,99 @@
+﻿Function Invoke-DBRestore {
+    
+    [cmdletbinding()]
+        Param(
+            [parameter(Mandatory=$true, ValueFromPipeline)]
+            [ValidateNotNullOrEmpty()] #No value
+            [string]$database
+            )
+            
+    Begin {
+        
+        <#
+            Helper function to show usage of cmdlet.
+        #>
+        Function Show-Usage {
+        
+            Show-InfoMessage "Usage: Invoke-DBRestore -database [db]"        
+            Show-InfoMessage "db: services for Mohawk_Services_Dev Database"
+            Show-InfoMessage "db: commercial for Mohawk_TMGCommercial Database"
+            Show-InfoMessage "db: mongo for Mohawk_Mongo_Data Database"
+        }
+    }
+    Process {
+        
+        $workingDir = (Get-Item -Path ".\" -Verbose).FullName
+        $databaseToRestore = "";
+        $dbBackupStorageRoot = if(![string]::IsNullOrEmpty($env:DBBACKUPSTORAGEROOT)) { $env:DBBACKUPSTORAGEROOT } else { "C:\stuff\DBBackups\" }
+        $dbBackupStorageDir = "";
+        $backupFileName = "";
+
+        switch($database)
+        {
+            "services"
+                {                     
+                    $databaseToRestore = "Mohawk_Services_Dev"
+                    $dbBackupStorageDir = "$($dbBackupStorageRoot)Mohawk_Services"
+                    break                    
+                }
+            "commercial"
+                {                     
+                    $databaseToRestore = "Mohawk_TMGCommercial"
+                    $dbBackupStorageDir = "$($dbBackupStorageRoot)TMGCommercial"
+                    break                    
+                }
+            "mongo"
+                {                     
+                    $databaseToRestore = "Mohawk_Mongo_Data"
+                    $dbBackupStorageDir = "$($dbBackupStorageRoot)Mohawk_Mongo_Data"
+                    break                    
+                }
+            
+
+            default {
+                Show-InfoMessage "Invalid Database"
+                Show-Usage
+                return
+            }
+        }
+
+        # First get the backup file name. This will be from the last backup done.
+        $backupFiles = Get-ChildItem -path $dbBackupStorageDir -File
+
+        $backupFileName = $backupFiles | sort LastWriteTime | select -last 1
+        
+        if(![string]::IsNullOrEmpty($backupFileName)) {
+
+            $backupFilePath = "$($dbBackupStorageDir)\$($backupFileName)"
+
+        
+            Show-InfoMessage ("Restoring " + $databaseToRestore + " Database from file: " + $backupFileName + "...")
+
+            # Second, restart SQL Server to drop any open connections.
+
+            $sqlServiceCommand = "net stop MSSQLSERVER"
+            Invoke-Expression -Command:$sqlServiceCommand
+        
+            $sqlServiceCommand = "net start MSSQLSERVER"
+            Invoke-Expression -Command:$sqlServiceCommand
+        
+            Add-Type -path "C:\Windows\assembly\GAC_MSIL\Microsoft.SqlServer.Smo\10.0.0.0__89845dcd8080cc91\Microsoft.SqlServer.Smo.dll"
+
+            $sqlServer = New-Object "Microsoft.SqlServer.Management.SMO.Server"
+        
+            if($sqlServer.databases[$databaseToRestore])
+            {
+                $sqlServer.databases[$databaseToRestore].Drop()
+            }
+
+            # Third, Restore the database.
+            $sqlServerName = $sqlServer.Name      
+
+            Restore-SqlDatabase -ServerInstance $sqlServerName -Database $databaseToRestore -BackupFile $backupFilePath
+
+            Show-InfoMessage ($databaseToRestore + " Database Restore Complete.")            
+        }
+
+        cd $workingDir        
+    }
+}
